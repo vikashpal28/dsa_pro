36c7c70bc4b199d0155d70283d1fd9300352ac5e
vikashpal28 <vp537285@gmail.com>
2025-07-09 12:56:26 +0530

    You are given an array Arr consisting of N integers. You need to find the equilibrium index of the array.
    An index is considered as an equilibrium index if the sum of elements of the array to the left of that index is equal to the sum of elements to the right of it.
    
    Note:
    
    1. The array follows 0-based indexing, so you need to return the 0-based index of the element.
    2. Note that the element at the equilibrium index won’t be considered for either left sum or right sum.
    3. If there are multiple indices which satisfy the given condition, then return the left-most index i.e if there are indices i,j,k…. which are equilibrium indices, return the minimum among them
    4. If no such index is present in the array, return -1.

--- Solution.java
+++ Solution.java
@@ -10,7 +10,6 @@ public class Solution {
 		}
 		return -1;
 	}
-	
 }
 
 // You have been given an array/list 'ARR' of integers. Your task is to find the second largest element present in the 'ARR'.
