98d80d2e482ad7d0464bf7f611ff2157ab856df8
vikashpal28 <vp537285@gmail.com>
2025-07-09 12:15:49 +0530

    You have been given an integer array/list 'ARR' of size 'N'. Write a solution to check if it could become non-decreasing by modifying at most 1 element.
    We define an array as non-decreasing, if ARR[i] <= ARR[i + 1] holds for every i (0-based) such that (0 <= i <= N - 2).

--- Solution.java
+++ Solution.java
@@ -10,6 +10,7 @@ public class Solution {
 		}
 		return -1;
 	}
+	
 }
 
 // You have been given an array/list 'ARR' of integers. Your task is to find the second largest element present in the 'ARR'.
