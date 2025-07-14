7a864b58dfddd507b4d09ddefec5123ede4cd8bb
vikashpal28 <vp537285@gmail.com>
2025-07-14 16:34:29 +0530

    You are given an N x M integer matrix. Your task is to modify this matrix in place so that if any cell contains the value 0, then all cells in the same row and column as that cell should also be set to 0.

---
+++ multidemension_array/solution4.java
@@ -0,0 +1,67 @@
+package multidemension_array;
+
+import java.util.Arrays;
+
+public class solution4 {
+
+     public static void setZeros(int matrix[][]) {
+        // Write your code here..
+        boolean colZero = false;
+        boolean rowZero = false;
+
+        for(int i =0;i<matrix.length;i++){
+            if(matrix[i][0] == 0){
+                colZero = true;
+            }
+        }
+
+        for(int j = 0;j<matrix[0].length;j++){
+            if(matrix[0][j] == 0){
+                rowZero = true;
+            }
+        }
+
+        for(int i = 1 ;i<matrix.length;i++ ){
+        for(int j = 1;j<matrix[0].length;j++){
+            if(matrix[i][j] == 0){
+          matrix[i][0] = 0;
+          matrix[0][j] = 0;
+            }
+        }
+        }
+
+        for(int i =1;i<matrix.length;i++){
+            for(int j =0;j<matrix[0].length;j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(colZero){
+            for(int i =0;i<matrix.length;i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+        if(rowZero){
+            for(int j =0;j<matrix[0].length;j++){
+                matrix[0][j] = 0;
+            }
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        int[][] num = { {1, 2, 3},
+  {4, 0, 6},
+  {7, 8, 9} };
+  setZeros(num);
+  for(int[] n : num){
+    System.out.println(Arrays.toString(n));
+  }
+
+
+    }
+    
+}
