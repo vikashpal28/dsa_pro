7a62fd46a7abf55e93ea225df6e6292651c30243
vikashpal28 <vp537285@gmail.com>
2025-07-14 13:27:52 +0530

    You are given a square matrix, return true if the matrix is symmetric otherwise return false.
    A symmetric matrix is that matrix whose transpose is equal to the matrix itself.

---
+++ multidemension_array/solution2.java
@@ -0,0 +1,26 @@
+package multidemension_array;
+
+public class solution2 {
+
+    public static boolean isMatrixSymmetric(int[][] matrix) {
+        // Write your code here.
+        if(matrix.length!= matrix[0].length) return false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+   
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1,2,3},
+            {2,4,5},
+            {3,5,8}
+        } ;
+     System.out.println(isMatrixSymmetric(matrix));
+    }
+}
