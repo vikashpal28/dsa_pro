ccbbeceae71c3ae5a0aba4a1ae5840fb07eec4bc
vikashpal28 <vp537285@gmail.com>
2025-07-21 17:01:41 +0530

    Selection sort is one of the sorting algorithms that works by repeatedly finding the minimum element from the unsorted part of the array and putting it at the beginning of the unsorted region of the array.
    You are given an unsorted array consisting of N non-negative integers. Your task is to sort the array in non-decreasing order using the Selection Sort algorithm.

---
+++ sorting/solution2.java
@@ -0,0 +1,27 @@
+package sorting;
+
+public class solution2 {
+    public static void selectionSort(int arr[], int n) {
+        for(int i = 0;i<n;i++){
+            int minIdex = i;
+            for(int j =i+1;j<n;j++){
+                if(arr[j]<arr[minIdex]){
+                    minIdex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIdex];
+            arr[minIdex] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] num = {1,4,5,6,3};
+        int n = 5;
+        selectionSort(num, n);
+
+        for(int value : num){
+            System.out.print(value + " ");
+        }
+
+    }
+}
